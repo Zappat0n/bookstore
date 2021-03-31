a4c3db5038a5e36b910ec0a586ce4c079c513d51
Amita-Roy <sardana.amita2@gmail.com>
2021-03-31 15:39:13 +0200

    Add provider, ceatestore in index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={createStore(rootReducer)}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
-
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers, createStore } from 'redux'
-import bookReducer from './books'
-
+import { combineReducers } from 'redux';
+import bookReducer from './books';
 
 const rootReducer = combineReducers({
   bookReducer,
-})
+});
 
+export default rootReducer;
