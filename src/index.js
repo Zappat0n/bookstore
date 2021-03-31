b87b1cedaf3caa62effd97c59d201e385c45b5b7
Zappat0n <albarros1973@gmail.com>
2021-03-31 16:33:14 +0200

    Create initialState

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ import { createStore } from 'redux';
 import rootReducer from './reducers';
 import App from './components/App';
 
+export const initialState = {
+  books: [
+    {
+      id: Math.random(),
+      title: 'Quixote',
+      category: 'Action'
+    },
+    {
+      id: Math.random(),
+      title: 'Hamlet',
+      category: 'Biography'
+    },
+    {
+      id: Math.random(),
+      title: 'The history of the universe',
+      category: 'Sci-Fi'
+    },
+  ]
+}
+
 ReactDOM.render(
   <Provider store={createStore(rootReducer)}>
     <React.StrictMode>
