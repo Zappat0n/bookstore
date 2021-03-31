ffbae060119899d2f86fcc26c97921c7d1116cc8
Zappat0n <albarros1973@gmail.com>
2021-03-31 16:32:59 +0200

    update bookReducer

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,5 +1,11 @@
+import { CREATE_BOOK, REMOVE_BOOK} from '../actions'
+
 const bookReducer = (state = [], action) => {
-  return state;
+  switch (action.type) {
+    case CREATE_BOOK: return [...state,action.payload];
+    case REMOVE_BOOK: return state.filter( (book) => book.id !== action.payload);
+    default: return state;
+  }
 };
 
 export default bookReducer;
