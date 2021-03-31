6498173a1f9db7f81873857aa0bc58c6d0a14459
Zappat0n <albarros1973@gmail.com>
2021-03-31 18:37:19 +0200

    Fix merging issues

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import App from './components/App';
+
+const initialState = {
+  bookReducer: [
+    {
+      id: Math.random(),
+      title: 'Quixote',
+      category: 'Action',
+    },
+    {
+      id: Math.random(),
+      title: 'Hamlet',
+      category: 'Biography',
+    },
+    {
+      id: Math.random(),
+      title: 'The history of the universe',
+      category: 'Sci-Fi',
+    },
+  ],
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={createStore(rootReducer, initialState)}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
