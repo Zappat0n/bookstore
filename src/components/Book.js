d46d459a143220c82d6ba59db78177b1a0742565
Zappat0n <albarros1973@gmail.com>
2021-04-01 10:45:17 +0200

    Add button to Book

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = (props) => {
-  const { book } = props;
+  const { book, handleChange } = props;
+
   return (
     <tr>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
+      <td><button type="button" onClick={(event) => handleChange(event, book)}>Remove Book</button></td>
     </tr>
   );
 };
 
 Book.propTypes = {
   book: PropTypes.instanceOf(Object).isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default Book;
