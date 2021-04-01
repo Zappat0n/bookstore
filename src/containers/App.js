5c25b303a33ff547b3de6856722d04227f11a8b6
Amita-Roy <sardana.amita2@gmail.com>
2021-04-01 16:00:57 +0200

    Add styles for navigation and fix the width for app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,15 @@
 import BooksList from './BooksList';
 import BookForm from './BookForm';
+import Navigation from '../components/navigation/Navigation';
+import './App.css';
 
 const App = () => (
-  <div>
-    <BooksList />
-    <BookForm />
+  <div className="app">
+    <Navigation />
+    <div className="main-content">
+      <BooksList />
+      <BookForm />
+    </div>
   </div>
 );
 
