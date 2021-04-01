3be7a6d08e2692e2eb21c6137ee2f8aea01b8947
Amita-Roy <sardana.amita2@gmail.com>
2021-04-01 18:27:18 +0200

    Update navigation style and project title

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './Navigation.css';
 
 const Navigation = () => (
-  <div className="navigation">
-    <div className="left-content">
-      <h2 className="brand-name">Bookstore CMS</h2>
-      <p className="ml">BOOKS</p>
-      <p className="ml" id="categories-tab">
-        CATEGORIES
-      </p>
-    </div>
-    <div className="right-content">
-      <div className="login-user">
-        <i className="fas fa-user" />
+  <div className="navigation-wraper">
+    <div className="navigation">
+      <div className="left-content">
+        <h2 className="brand-name">Bookstore CMS</h2>
+        <p className="ml">BOOKS</p>
+        <p className="ml" id="categories-tab">
+          CATEGORIES
+        </p>
+      </div>
+      <div className="right-content">
+        <div className="login-user">
+          <i className="fas fa-user" />
+        </div>
       </div>
     </div>
   </div>
